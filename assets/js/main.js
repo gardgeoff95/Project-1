793b216ccb0c04863a8b708e1d20e6829a546b7f
Geoff Gard <gardgeoff95@email.arizona.edu>
2019-06-25 11:38:20 -0700

    all js for questions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 $("document").ready(function () {
+    var player;
+
+
+    if (localStorage.getItem("player") != null) {
+        player = localStorage.getItem("player");
+        console.log(player);
+    }
     var firebaseConfig = {
         apiKey: "AIzaSyA45eFTNK6tPmxWAEseW3BSqNEgwpm9JmQ",
         authDomain: "project-1-f43b7.firebaseapp.com",
@@ -15,47 +22,47 @@ $("document").ready(function () {
 
 
     //creating main page js
-    function setState(player, state){
-        database.ref("-Li-SK5eziAtIuM4aLJJ/playerState/p" + player +"Taken").set(state);
+    function setState(player, state) {
+        database.ref("-Li-SK5eziAtIuM4aLJJ/playerState/p" + player + "Taken").set(state);
 
     }
     //2 on click functions to set database calls
 
     $("#p1Choice").on("click", function () {
 
-        setState(1,"yes");
-        localStorage.setItem("player","1");
+        setState(1, "yes");
+        localStorage.setItem("player", "1");
     });
     $("#p2Choice").on("click", function () {
-        setState(2,"yes");
-        localStorage.setItem("player","2");
+        setState(2, "yes");
+        localStorage.setItem("player", "2");
     })
 
     //checks to see whene data changes and sets visibility to hidden;
     database.ref("-Li-SK5eziAtIuM4aLJJ/playerState").on("value", function (snap) {
         if (snap.val().p1Taken == "yes") {
             $("#p1Choice").css("visibility", "hidden");
-        } 
-        
+        }
+
         console.log(snap.val().p2Taken);
         if (snap.val().p2Taken == "yes") {
             $("#p2Choice").css("visibility", "hidden");
-        } 
-        
-        
-        if ((snap.val().p1Taken =="yes")&&(snap.val().p2Taken)=="yes"){
-             window.location.href = "category.html"
+        }
+
+
+        if ((snap.val().p1Taken == "yes") && (snap.val().p2Taken) == "yes") {
+            window.location.href = "category.html"
             console.log(window.location.href);
-            setState(1,"no");
-            setState(2,"no");
-           
-            
+            setState(1, "no");
+            setState(2, "no");
+
+
 
         }
 
 
 
-        
+
 
 
 
@@ -65,86 +72,127 @@ $("document").ready(function () {
     //end main page JS
     //start category JS
 
-    $(".categories").on("click",function(){
-        
+    $(".categories").on("click", function () {
+
         var categoryName = ($(this).attr("category"))
 
 
-        if(categoryName == "animals"){
+        if (categoryName == "animals") {
             database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/category").set(27);
 
-        } else if (categoryName == "sports"){
+        } else if (categoryName == "sports") {
             database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/category").set(21);
-        } else if (categoryName == "genKnowledge"){
+        } else if (categoryName == "genKnowledge") {
             database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/category").set(9);
-        } else if (categoryName == "vehicles"){
+        } else if (categoryName == "vehicles") {
             database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/category").set(28);
 
         }
 
-        
+
 
 
     });
 
 
 
-    $(".diffButton1").on("click",function(){
-     
-         database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/difficulty").set($(this).text());
-     
+    $(".diffButton1").on("click", function () {
+
+        database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/difficulty").set($(this).text());
+
     })
 
-    $(".quesButton1").on("click",function(){
+    $(".quesButton1").on("click", function () {
         var button = this;
-     
-            database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/length").set($(this).text());
-    
+
+        database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/length").set($(this).text());
+
     })
 
 
 
-    $("#submitButtonCat").on("click",function(){
-        window.location.href="index.html"
+    $("#submitButtonCat").on("click", function () {
+        window.location.href = "index.html"
     });
-//end category JS
+    //end category JS
 
-//start questions JS
-database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/").on("value",function(snapshot){
-    console.log(snapshot.val());
-    category = snapshot.val().category;
-    amount = snapshot.val().length;
-    difficulty = snapshot.val().difficulty;
-    queryURL = "https://opentdb.com/api.php?amount="+amount +"&category="+ category +"&difficulty="+ difficulty +"&type=multiple"
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        results = response.results;
-        console.log(response);
-           
-        
-            for(i= 0; i < amount; i++){
-
-
-                
-                var answers = [results[i].incorrect_answers[0], results[i].incorrect_answers[1], results[i].incorrect_answers[2]];
+    //start questions JS
+    database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/").on("value", function (snapshot) {
+        console.log(snapshot.val());
+        category = snapshot.val().category;
+        amount = snapshot.val().length;
+        difficulty = snapshot.val().difficulty;
+        queryURL = "https://opentdb.com/api.php?amount=" + amount + "&category=" + category + "&difficulty=" + difficulty + "&type=multiple"
 
 
 
-                
-                
 
-                $("#question").text(decodeURI(results[i].question));
-                $("#choiceA").text(results[i].correct_answer);
-                $("#choiceB").text(answers[0]);
-                $("#choiceC").text(answers[2]);
-                $("#choiceD").text(answers[1]);
-                break;
+        var p1Right = 0;
+        var p2Right = 0;
+
+
+
+
+        $(".choiceButton").on("click", function () {
+            if (player == 1) {
+                if ($(this).text() == "A.)") {
+                    p1Right++;
+                    database.ref("questions/p1rightanswers").set(p1Right);
+                }
+            } else if (player == 2) {
+                if ($(this).text() == "A.)") {
+                    p2Right++;
+                    database.ref("questions/p2rightanswers").set(p2Right);
+                }
             }
 
-           
+
+        });
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            results = response.results;
+            console.log(results.length);
+            var questionArray = []
+            var randomSelector = ["choiceA", "choiceB", "choiceC", "choiceD"];
+
+            for (i = 0; i < results.length; i++) {
+                questionArray.push(results[i]);
+                console.log(questionArray);
+
+            }
+            function generateQuestions(questionNumb) {
+                var tempSelec = randomSelector;
+                var tempQuestionOrder = []
+
+                for (i = 0; i < 4; i++) {
+                    var ri = Math.floor(Math.random() * tempSelec.length);
+                    var rs = tempSelec.splice(ri, 1);
+                    tempQuestionOrder.push(rs);
+                    
+                }
+                $("#submitButton").on("click",function(){
+
+                   
+                    $("#question").html(results[questionNumb].question);
+                    $("#" + tempQuestionOrder[0]).html(results[questionNumb].incorrect_answers[0]);
+                    $("#"+ tempQuestionOrder[1]).html(results[questionNumb].incorrect_answers[1]);
+                    $("#" + tempQuestionOrder[2]).html(results[questionNumb].incorrect_answers[2]);
+                    $("#" + tempQuestionOrder[3]).html(results[questionNumb].correct_answer);
+
+                })
+
+
+                
+
+            }
+            generateQuestions(0);
+
+
+
+
+
         });
 
 
@@ -157,9 +205,9 @@ database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/").on("value",function(snapshot){
 
 
 
-      
+
     });
-    
+
 });
 
 
@@ -167,32 +215,3 @@ database.ref("-Li6vJmWPUCPZGVlKj0W/Categories/").on("value",function(snapshot){
 
 
 //end questions JS
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
